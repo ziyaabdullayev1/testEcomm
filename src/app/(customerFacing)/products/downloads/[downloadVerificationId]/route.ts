8755c1449764b5fde3361144b3ed04dee1b05f9a
ziyaabdullayev1 <ziya.abdullayev@std.yildiz.edu.tr>
2024-06-10 19:17:23 +0300

    7-17

---
+++ src/app/(customerFacing)/products/downloads/[downloadVerificationId]/route.ts
@@ -0,0 +1,36 @@
+import db from "@/db/db"
+import { NextRequest, NextResponse } from "next/server"
+import fs from "fs/promises"
+
+
+export function GET(){
+  console.log("Hello World")
+  return new NextResponse("Hello World")
+
+}
+// export async function GET(
+//   req: NextRequest,
+//   {
+//     params: { downloadVerificationId },
+//   }: { params: { downloadVerificationId: string } }
+// ) {
+//   const data = await db.downloadVerification.findUnique({
+//     where: { id: downloadVerificationId, expiresAt: { gt: new Date() } },
+//     select: { product: { select: { filePath: true, name: true } } },
+//   })
+
+//   if (data == null) {
+//     return NextResponse.redirect(new URL("/products/download/expired", req.url))
+//   }
+
+//   const { size } = await fs.stat(data.product.filePath)
+//   const file = await fs.readFile(data.product.filePath)
+//   const extension = data.product.filePath.split(".").pop()
+
+//   return new NextResponse(file, {
+//     headers: {
+//       "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
+//       "Content-Length": size.toString(),
+//     },
+//   })
+// }
